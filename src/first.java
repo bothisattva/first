841955c1fcc4830835574407ec7282842d5c2c13
bothisattva <orlov21423@mail.ru>
2018-12-26 13:00:46 +0300

    3 метод вычисляющий выражение

--- src/first.java
+++ src/first.java
@@ -1,5 +1,7 @@
 public class first {
+    //задача 1
     public static void main(String[] args) {
+       /*  задача 2
         byte a = 1;
         int b = 2;
         short c = 3;
@@ -8,10 +10,18 @@ public class first {
         boolean flsg = true;
         String str = "строка";
         char d = 'a';
+        */
 
-
-
-
-
+            /* задача 3
+            System.out.println(sum(2,3,4,4));
+            */
     }
+            /*3задача 3
+            public static int sum (int a, int b , int c, int d){
+              int s ;
+                s = a*(b+(c/d));
+             return s;
+
+             }
+             */
 }
