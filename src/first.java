a6bc0448c9458b99be1e8075d1d62806b53040a0
bothisattva <orlov21423@mail.ru>
2018-12-26 12:38:24 +0300

    2 переменный всех типов данных

--- src/first.java
+++ src/first.java
@@ -1,5 +1,17 @@
 public class first {
     public static void main(String[] args) {
+        byte a = 1;
+        int b = 2;
+        short c = 3;
+        double e = 4.12f;
+        float  f = 5.1f;
+        boolean flsg = true;
+        String str = "строка";
+        char d = 'a';
+
+
+
+
 
     }
 }
